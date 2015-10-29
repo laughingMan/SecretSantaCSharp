1e6d5abe2d7b5cc63437ebcdc4b2e9176c9be83c
unknown <isaac.hatton@ihatton-ml1.schafer.lan>
2015-10-29 12:31:43 -0600

    project setup

---
+++ SecretSantaCSharp/Tests/GeneratorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+
+namespace SecretSantaCSharp.Tests
+{
+    [TestFixture]
+    public class GeneratorTests
+    {
+        [Test]
+        public void testWhenTwoPeopleAreListedThenTheyAreBothSantasOfEachOther()
+        {
+            
+        }
+    }
+}
+
+ 
